aa7e7b9cf6c983367c439838bd4343c370315bf4
Mars <nodexchange@outlook.com>
2023-07-14 15:59:13 +0100

    seo for gallery

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { NextSeo } from "next-seo";
 import NextImage from "next/image";
 import Link from "next/link";
 import cloudinary from "@/utils/cloudinary";
@@ -114,115 +115,118 @@ const Gallery = ({ images }) => {
   };
 
   return (
-    <main className="relative">
-      <Dialog open={open} onOpenChange={setOpen}>
-        {photoId && (
-          <DialogPortal>
-            <DialogOverlay className="fixed inset-0 bg-black/75 backdrop-blur-md rdx-state-open:overlay-fade-in rdx-state-closed:overlay-fade-out" />
-            <DialogContent
-              className="fixed inset-0 mx-auto my-auto rounded shadow outline-none h-fit w-fit rdx-state-open:dialog-item-open rdx-state-closed:dialog-item-close"
-              onEscapeKeyDown={() => router.back()}
-              onPointerDownOutside={() => router.back()}
-            >
-              <div
-                className={cx(
-                  "relative w-auto sm:h-[800px]",
-                  selectedImage.width < selectedImage.height
-                    ? "h-[500px]"
-                    : "h-[250px]"
-                )}
+    <>
+      <NextSeo title="Our Work" canonical="https://redkiteaerial.co.uk/gallery" />
+      <main className="relative">
+        <Dialog open={open} onOpenChange={setOpen}>
+          {photoId && (
+            <DialogPortal>
+              <DialogOverlay className="fixed inset-0 bg-black/75 backdrop-blur-md rdx-state-open:overlay-fade-in rdx-state-closed:overlay-fade-out" />
+              <DialogContent
+                className="fixed inset-0 mx-auto my-auto rounded shadow outline-none h-fit w-fit rdx-state-open:dialog-item-open rdx-state-closed:dialog-item-close"
+                onEscapeKeyDown={() => router.back()}
+                onPointerDownOutside={() => router.back()}
               >
-                <NextImage
-                  alt=""
-                  src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_1440/${selectedImage.public_id}.${selectedImage.format}`}
-                  placeholder="blur"
-                  blurDataURL={selectedImage.blurDataURL}
-                  fill={true}
-                  style={{objectFit: "contain"}}
-                  className="rounded shadow-md !relative"
-                />
-              </div>
-              <div className="flex gap-x-2 absolute top-14 right-4">
-                {/* <button>Share</button> */}
-                <DialogClose
-                  className="rounded-full bg-white/10 text-black p-2.5 leading-none hover:bg-white transition shrink-0"
-                  onClick={() => router.back()}
+                <div
+                  className={cx(
+                    "relative w-auto sm:h-[800px]",
+                    selectedImage.width < selectedImage.height
+                      ? "h-[500px]"
+                      : "h-[250px]"
+                  )}
                 >
-                  <X />
-                </DialogClose>
-              </div>
-            </DialogContent>
-          </DialogPortal>
-        )}
-      </Dialog>
-      <Masonry
-        breakpointCols={breakpointColumnsObj}
-        className="my-masonry-grid py-4 px-8 sm:px-4"
-        columnClassName="my-masonry-grid_column"
-      >
-        <div className="h-96 border border-black rounded px-4 pt-2 pb-5 flex flex-col justify-between">
-          <Heading>Our Work</Heading>
-          <div>
-            <p className="text-sm mb-2">
-              Step into a captivating world of breathtaking aerials and mesmerising perspectives. Here, we proudly showcase our exceptional aerial photography and video masterpieces tailored exclusively for real estate and Airbnb listings.  From sprawling estates with panoramic views to cozy Airbnb retreats nestled amidst nature&apos;s embrace, each photograph and video showcases the distinctive charm and allure of the properties we have had the privilege to capture.
-            </p>
-            {/* <div className="flex gap-0.5 flex-wrap">
-              {FILTERS.map(({ filter, type, title }) => (
-                <FilterTag
-                  key={type}
-                  filter={type}
-                  onClick={() => setFilter(filter)}
-                >
-                  {title}
-                </FilterTag>
-              ))}
-            </div> */}
+                  <NextImage
+                    alt=""
+                    src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_1440/${selectedImage.public_id}.${selectedImage.format}`}
+                    placeholder="blur"
+                    blurDataURL={selectedImage.blurDataURL}
+                    fill={true}
+                    style={{objectFit: "contain"}}
+                    className="rounded shadow-md !relative"
+                  />
+                </div>
+                <div className="flex gap-x-2 absolute top-14 right-4">
+                  {/* <button>Share</button> */}
+                  <DialogClose
+                    className="rounded-full bg-white/10 text-black p-2.5 leading-none hover:bg-white transition shrink-0"
+                    onClick={() => router.back()}
+                  >
+                    <X />
+                  </DialogClose>
+                </div>
+              </DialogContent>
+            </DialogPortal>
+          )}
+        </Dialog>
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="my-masonry-grid py-4 px-8 sm:px-4"
+          columnClassName="my-masonry-grid_column"
+        >
+          <div className="h-96 border border-black rounded px-4 pt-2 pb-5 flex flex-col justify-between">
+            <Heading>Our Work</Heading>
+            <div>
+              <p className="text-sm mb-2">
+                Step into a captivating world of breathtaking aerials and mesmerising perspectives. Here, we proudly showcase our exceptional aerial photography and video masterpieces tailored exclusively for real estate and Airbnb listings.  From sprawling estates with panoramic views to cozy Airbnb retreats nestled amidst nature&apos;s embrace, each photograph and video showcases the distinctive charm and allure of the properties we have had the privilege to capture.
+              </p>
+              {/* <div className="flex gap-0.5 flex-wrap">
+                {FILTERS.map(({ filter, type, title }) => (
+                  <FilterTag
+                    key={type}
+                    filter={type}
+                    onClick={() => setFilter(filter)}
+                  >
+                    {title}
+                  </FilterTag>
+                ))}
+              </div> */}
+            </div>
           </div>
-        </div>
 
-        {gridImages.map(
-          ({ id, public_id, format, width, height, blurDataUrl }) => (
-            <AnimatePresence key={id}>
-              <Link
-                href={`/gallery/?photoId=${id}`}
-                as={`/gallery/${id}`}
-              >
-                <MotionImage
-                  // initial={{ scale: 0.8, opacity: 0 }}
-                  // animate={{
-                  //   scale: 1,
-                  //   opacity: 1,
-                  // }}
-                  // exit={{
-                  //   scale: 0.8,
-                  //   opacity: 0,
-                  // }}
-                  // layout
-                  onClick={() => {
-                    setSelectedImage({
-                      public_id: public_id,
-                      format: format,
-                      alt: "",
-                      blurDataURL: blurDataUrl,
-                      width: width,
-                      height: height,
-                    });
-                    setOpen(true);
-                  }}
-                  className="cursor-pointer block overflow-hidden transition-all duration-500 border rounded-lg shadow betterhover:hover:shadow-xl betterhover:hover:shadow-yolk/50 betterhover:hover:border-yolk border-kite"
-                  alt=""
-                  placeholder="blur"
-                  blurDataURL={blurDataUrl}
-                  src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
-                  width={width}
-                  height={height}
-                />
-              </Link>
-            </AnimatePresence>
-          )
-        )}
-      </Masonry>
-    </main>
+          {gridImages.map(
+            ({ id, public_id, format, width, height, blurDataUrl }) => (
+              <AnimatePresence key={id}>
+                <Link
+                  href={`/gallery/?photoId=${id}`}
+                  as={`/gallery/${id}`}
+                >
+                  <MotionImage
+                    // initial={{ scale: 0.8, opacity: 0 }}
+                    // animate={{
+                    //   scale: 1,
+                    //   opacity: 1,
+                    // }}
+                    // exit={{
+                    //   scale: 0.8,
+                    //   opacity: 0,
+                    // }}
+                    // layout
+                    onClick={() => {
+                      setSelectedImage({
+                        public_id: public_id,
+                        format: format,
+                        alt: "",
+                        blurDataURL: blurDataUrl,
+                        width: width,
+                        height: height,
+                      });
+                      setOpen(true);
+                    }}
+                    className="cursor-pointer block overflow-hidden transition-all duration-500 border rounded-lg shadow betterhover:hover:shadow-xl betterhover:hover:shadow-yolk/50 betterhover:hover:border-yolk border-kite"
+                    alt=""
+                    placeholder="blur"
+                    blurDataURL={blurDataUrl}
+                    src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
+                    width={width}
+                    height={height}
+                  />
+                </Link>
+              </AnimatePresence>
+            )
+          )}
+        </Masonry>
+      </main>
+    </>
   );
 };
 
