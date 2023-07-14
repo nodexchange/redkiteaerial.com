99fd01b486eef688988c107748477eb4d2ed63c1
Mars <nodexchange@outlook.com>
2023-07-14 15:14:49 +0100

    seo play

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ const MyApp = ({ Component, pageProps }) => {
             title={undefined}
             titleTemplate="%s | Red Kite Aerial"
             defaultTitle="Red Kite Aerial | Drone Photography and Video Service"
-            description="Aerial Services based in Radnage, Buckinghamshire, UK specialising in real estate, airbnb listing and commercial property footage capture."
+            description="Premium Photo and Video services for breathtaking Aerial Photography and captivating Video services based in Radnage, Buckinghamshire, UK. We specialise in Real Estate & Airbnb listing as well as commercial property footage capture. Check our affordable services and book now!"
             canonical="https://redkiteaerial.co.uk"
             twitter={{
               handle: "@redkiteaerial",
@@ -31,7 +31,7 @@ const MyApp = ({ Component, pageProps }) => {
               locale: "en_CA",
               url: "https://redkiteaerial.co.uk",
               title: "Red Kite Aerial | Drone Photography and Video Service",
-              description: "Aerial Services based in Radnage, Buckinghamshire, UK specialising in real estate, airbnb listing and commercial property footage capture.",
+              description: "Premium Photo and Video services for breathtaking Aerial Photography and captivating Video services based in Radnage, Buckinghamshire, UK. We specialise in Real Estate & Airbnb listing as well as commercial property footage capture. Check our affordable services and book now!",
               images: [
                 {
                   url: "https://redkiteaerial.co.uk/images/og.png",
